7469aa7d13e0a78e38d3af281ec62bba7c7c3652
Alexander Kacharava <alexander@muze.nl>
2022-08-09 09:47:55 +0200

    Remove unecesary code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello world!
-      </header>
+    <div>
+      <h2>Hello world!</h2>
     </div>
   );
 }
