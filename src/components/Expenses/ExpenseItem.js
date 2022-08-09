f5959eac5b335469766f857fd7cc7ca84216a300
Alexander Kacharava <alexander@muze.nl>
2022-08-09 14:10:12 +0200

    Add state management to ExpenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,26 @@
+import React, { useState } from 'react';
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from '../UI/Card';
 
 function ExpenseItem(props){
+  const [title, setTitle] = useState(props.title);
+
+  let amount = props.amount;
+  let date = props.date;
+
   function clickHandler() {
+    setTitle('Updated!');
     console.log('Clicked!');
   }
 
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date} />      
+      <ExpenseDate date={date} />      
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
     </Card>
