9e4a37baa82d9ea1f130b5bf6f8a99531bb28cfb
Alexander Kacharava <alexander@muze.nl>
2022-08-11 10:53:00 +0200

    Improve logic for filtering

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -19,22 +19,28 @@ function ExpenseList(props) {
     return expense.date.getFullYear().toString() === yearInput.filteredYear;
   })
 
+  let expensesContent = <p>No Expenses found.</p>;
+
+  if(filteredExpenses.length > 0){
+    expensesContent = filteredExpenses.map(function (expense) {
+      return (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      );
+    })
+  }
+
   return (
     <Card className="expenses">
       <ExpenseFilter
         selectedYear={yearInput.filteredYear}
         onFilterExpenseYear={filterExpenseYearHandler}
       />
-      {filteredExpenses.map(function (expense) {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        );
-      })}
+      {expensesContent}
     </Card>
   );
 }
