ca537b129de72b53c0690e75565d6b39b856ed67
Alexander Kacharava <alexander@muze.nl>
2022-08-11 09:52:31 +0200

    Render a list of expense items

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,28 +1,15 @@
-import React, { useState } from 'react';
-
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from '../UI/Card';
 
 function ExpenseItem(props){
-  const [title, setTitle] = useState(props.title);
-
-  let amount = props.amount;
-  let date = props.date;
-
-  function clickHandler() {
-    setTitle('Updated!');
-    console.log('Clicked!');
-  }
-
   return (
     <Card className="expense-item">
-      <ExpenseDate date={date} />      
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -15,34 +15,23 @@ function ExpenseList(props) {
     });
   }
 
-  console.log(yearInput);
-
   return (
     <Card className="expenses">
       <ExpenseFilter
         selectedYear={yearInput.filteredYear}
         onFilterExpenseYear={filterExpenseYearHandler}
       />
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
+      {
+        props.expenses.map(function(expense){
+          return(
+            <ExpenseItem
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          );
+        })
+      }
     </Card>
   );
 }
