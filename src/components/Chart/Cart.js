77fd79f680fe31f6f729f88a9b80e18104a1f439
Alexander Kacharava <alexander@muze.nl>
2022-08-11 13:14:59 +0200

    Add dynamic styles

---
+++ src/components/Chart/Cart.js
@@ -0,0 +1,23 @@
+import ChartBar from './ChartBar';
+import './Chart.css';
+
+function Chart(props){
+  return(
+    <div className='chart'>
+      {
+        props.dataPoints.map(function(dataPoint){
+          return (
+            <ChartBar
+              key={dataPoint.label}
+              value={dataPoint.value}
+              maxValue={null}
+              label={dataPoint.label}
+            />
+          );
+        })
+      }
+    </div>
+  );
+}
+
+export default Chart;
