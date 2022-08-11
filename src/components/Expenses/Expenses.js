071ddf7fe413982179ef106bc6ec6202d04de1eb
Alexander Kacharava <alexander@muze.nl>
2022-08-11 11:17:27 +0200

    Create new component for rendering expenses with logic

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpenseFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
   const [yearInput, setYearInput] = useState({
@@ -19,28 +19,13 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === yearInput.filteredYear;
   })
 
-  let expensesContent = <p>No Expenses found.</p>;
-
-  if(filteredExpenses.length > 0){
-    expensesContent = filteredExpenses.map(function (expense) {
-      return (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      );
-    })
-  }
-
   return (
     <Card className="expenses">
       <ExpenseFilter
         selectedYear={yearInput.filteredYear}
         onFilterExpenseYear={filterExpenseYearHandler}
       />
-      {expensesContent}
+      <ExpensesList expenses={filteredExpenses} />
     </Card>
   );
 }
